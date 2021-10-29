ca8692f1c981b300fdcc03964027c2e9965b6b49
Jose Sancheza <sanchezjoserafael25@gmail.com>
2021-10-28 20:18:20 -0400

    Implented cloudinary to save the images of the users, and made a tille preview of the uploaded images, It still need improvements

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,30 +1,65 @@
 import React, { useState } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
+import { FormControl, FilledInput, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { postMessage } from "../../store/utils/thunkCreators";
+import { AddPhotoAlternateOutlined } from "@material-ui/icons";
+
+const cloudinaryURI = "https://api.cloudinary.com/v1_1/jrscloud/upload";
 
 const useStyles = makeStyles(() => ({
   root: {
     justifySelf: "flex-end",
-    marginTop: 15
+    marginTop: 15,
   },
   input: {
     height: 70,
     backgroundColor: "#F4F6FA",
     borderRadius: 8,
-    marginBottom: 20
-  }
+    marginBottom: 20,
+  },
 }));
 
 const Input = (props) => {
   const classes = useStyles();
   const [text, setText] = useState("");
+  const [images, setImages] = useState([]);
   const { postMessage, otherUser, conversationId, user } = props;
 
   const handleChange = (event) => {
     setText(event.target.value);
   };
+
+  const saveImage = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "lrjzbnj9");
+      const response = await fetch(cloudinaryURI, {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChangeImages = async (event) => {
+    const files = event.target.files;
+    const newImages = await Promise.all(
+      Array.from(files).map(async (file) => {
+        return await saveImage(file);
+      })
+    );
+
+    if (newImages) {
+      setImages((old) => [...old, ...newImages]);
+    }
+  };
+
+  const resetImages = () => setImages([]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -33,25 +68,48 @@ const Input = (props) => {
       text: event.target.text.value,
       recipientId: otherUser.id,
       conversationId,
-      sender: conversationId ? null : user
+      sender: conversationId ? null : user,
+      attachments: images,
     };
     await postMessage(reqBody);
     setText("");
   };
 
   return (
-    <form className={classes.root} onSubmit={handleSubmit}>
-      <FormControl fullWidth hiddenLabel>
-        <FilledInput
-          classes={{ root: classes.input }}
-          disableUnderline
-          placeholder="Type something..."
-          value={text}
-          name="text"
-          onChange={handleChange}
-        />
-      </FormControl>
-    </form>
+    <div>
+      <Box>
+        {images.map((image) => (
+          <img src={image.url} key={image.asset_id} />
+        ))}
+      </Box>
+      <form className={classes.root} onSubmit={handleSubmit}>
+        <FormControl fullWidth hiddenLabel>
+          <FilledInput
+            classes={{ root: classes.input }}
+            disableUnderline
+            placeholder="Type something..."
+            value={text}
+            name="text"
+            onChange={handleChange}
+            endAdornment={
+              <>
+                <label htmlFor="image-upload">
+                  <AddPhotoAlternateOutlined color="secondary" />
+                </label>
+                <input
+                  multiple
+                  accept="image/*"
+                  id="image-upload"
+                  hidden
+                  type="file"
+                  onChange={handleChangeImages}
+                />
+              </>
+            }
+          />
+        </FormControl>
+      </form>
+    </div>
   );
 };
 
